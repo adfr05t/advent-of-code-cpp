7080f3aa3964220004a232f1f841e5bf37139dba
Adam Frost <adam.frost1@outlook.com>
2026-07-26 17:43:58 +0100

    chore: initialise C++ Advent of Code project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code C++
+
+My Advent of Code solutions written in C++.
---
+++ src/2025/day-01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "AOC project is working\n";
+
+    return 0;
+}
